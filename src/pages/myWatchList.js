873d5dc5ba312a6149aac869adcf18829bad6ae2
chaikovskyit <chaikovskyit@gmail.com>
2021-03-12 17:35:46 +0200

    watch list

--- src/pages/myWatchList.js
+++ src/pages/myWatchList.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { InputGroup, Button, Input } from 'reactstrap';
+import { v4 as uuidv4 } from 'uuid'
 
-const myWatchList = () => {
+const MyWatchList = () => {
+
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const newItem = () => {
+    if(item.trim() !== ''){
+      const newItem = {
+        id: uuidv4(),
+        item: item,
+      }
+      setItems((items) => [...items, newItem])
+      setItem('')
+    } else {
+      alert('You forgot enter episode...')
+      setItem('')
+    }
+  }
+  const deleteItem = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+
   return(
-    <h1>My watch list page</h1>
+    <div className='myWatchList'>
+      <InputGroup className='inputGroup'>
+        <Input 
+          value={item}
+          className='input' 
+          placeholder='Enter episode....'
+          onChange={(e) => setItem(e.target.value)}
+        />
+        <Button 
+          className='button' 
+          color="secondary"
+          onClick={newItem}
+        >
+          Enter
+        </Button>
+      </InputGroup>
+      <h2>View later</h2>
+      <ul className='watchList'>
+        {
+          items.map((item) => {
+            return (
+              <li>{item.item}
+                <Button 
+                  outline 
+                  color="danger"
+                  onClick={() => deleteItem(item.id)}
+                >
+                  delete
+                </Button>
+              </li>)
+          })
+        }
+      </ul>
+      
+      
+    </div>
   )
 }
 
-export default myWatchList
+export default MyWatchList
